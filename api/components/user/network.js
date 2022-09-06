84b6eababeee9d72a3dd7120221a83ef5e392b85
Jhon Arcila Castano <jhon380172793@ucaldas.edu.co>
2022-09-06 15:36:19 -0400

    swagger implement and doc for user

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -4,6 +4,19 @@ import { success, error } from '../../../network/response.js';
 
 const router = express.Router();
 
+/**
+ * User model
+ * @typedef {object} User
+ * @property {string} name.required - user name
+ * @property {integer} id - User id- int64
+ */
+
+/**
+ * GET /api/users
+ * @summary Get all users
+ * @tags Users
+ * @return {array<User>} 200 - success response - application/json
+ */
 router.get('/', (req, res) => {
     controller.listUsers().then((list) => {
         success(req, res, list, 200);
@@ -12,6 +25,13 @@ router.get('/', (req, res) => {
     });
 });
 
+/**
+ * GET /api/users/{id}
+ * @summary Get user by id
+ * @tags Users
+ * @param {integer} id.path.required - User id - int64
+ * @return {User} 200 - success response - application/json
+ */
 router.get('/:id', (req, res) => {
     controller.getUser(req.params.id).then((user) => {
         success(req, res, user, 200);
@@ -20,6 +40,13 @@ router.get('/:id', (req, res) => {
     });
 });
 
+/**
+ * POST /api/users
+ * @summary Create a new user
+ * @tags Users
+ * @param {User} request.body.required - users info - multipart/form-data
+ * @return {object} 200 - User created
+ */
 router.post('/', (req, res) => {
     controller.createUser(req.body).then((user) => {
         success(req, res, user, 201);
@@ -28,6 +55,31 @@ router.post('/', (req, res) => {
     });
 });
 
+/**
+ * PUT /api/users/{id}
+ * @summary Update user by id
+ * @tags Users
+ * @param {integer} id.path.required - User id - int64
+ * @param {User} request.body.required - users info - multipart/form-data
+ * @return {object} 200 - User updated
+ */
+router.put('/:id', (req, res) => {
+    controller.updateUser(req.params.id, req.body).then((user) => {
+        success(req, res, user, 200);
+    }).catch((err) => {
+        error(req, res, err, 500);
+    });
+});
+
+/**
+ * DELETE /api/users/{id}
+ * @summary Delete user by id
+ * @tags Users
+ * @param {integer} id.path.required - User id - int64
+ * @return {object} 200 - User deleted
+ * @return {object} 404 - User not found
+ * @return {object} 500 - Internal server error
+ */
 router.delete('/:id', (req, res) => {
     controller.deleteUser(req.params.id).then((user) => {
         success(req, res, user, 200);
--- api/index.js
+++ api/index.js
@@ -1,11 +1,48 @@
 import express from 'express';
 import morgan from 'morgan';
 import config from '../config.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+import expressJSDocSwagger from 'express-jsdoc-swagger'
+
+const options = {
+  info: {
+    version: '1.0.0',
+    title: 'Social Network API Documentation',
+    license: {
+      name: 'MIT',
+    },
+  },
+  security: {
+    BasicAuth: {
+      type: 'http',
+      scheme: 'basic',
+    },
+  },
+  baseDir: __dirname,
+  // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
+  filesPattern: './**/*.js',
+};
+
+expressJSDocSwagger(app)(options);
+
+/**
+ * GET /api/v1
+ * @summary This is the summary of the endpoint
+ * @return {object} 200 - success response
+ */
+app.get('/api/v1', (req, res) => res.json({
+  success: true,
+}));
 
 //Router configuration
 import user from './components/user/network.js';
